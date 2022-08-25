fd8b6e95b972d575cb1064238d6c43ed6e3a934f
NasihNazeem <nasihnazeem@gmail.com>
2022-08-25 13:42:40 -0600

    Rust is now working

---
+++ rust-practice/hello-world/target/debug/deps/hello_world.d
@@ -0,0 +1,5 @@
+H:\Github\NasihNazeem\Solana-Practice\rust-practice\hello-world\target\debug\deps\hello_world.exe: src\main.rs
+
+H:\Github\NasihNazeem\Solana-Practice\rust-practice\hello-world\target\debug\deps\hello_world.d: src\main.rs
+
+src\main.rs:
---
+++ rust-practice/hello-world/target/debug/hello-world.d
@@ -0,0 +1,1 @@
+H:\Github\NasihNazeem\Solana-Practice\rust-practice\hello-world\target\debug\hello-world.exe: H:\Github\NasihNazeem\Solana-Practice\rust-practice\hello-world\src\main.rs
